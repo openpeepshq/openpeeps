05d7cd591b347717b3e6a8ae579de6cd3620ee68
Jeremiah-Quaynor <hommeyj7@gmail.com>
2026-05-11 15:50:14 +0000

    chore(react-native): bump version to 0.2.6 and simplify Notifications component structure

--- platform/react-native/src/screens/(tabs)/notification/index.tsx
+++ platform/react-native/src/screens/(tabs)/notification/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { TabScreensHeader } from '~/components/custom';
 import { ThemedView } from '~/components/ui/themed-view';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs';
 import { ThemedText } from '~/components/ui/themed-text';
 import { View } from 'react-native';
 import { useOpenpeeps } from '@openpeeps/react';
@@ -10,14 +9,14 @@ import { NotificationFeed } from '~/components/custom/notification/NotificationF
 import { setAppBadgeCount } from '~/lib/notification-helpers';
 
 export const Notifications = () => {
-  const [tabValue, setTabValue] = useState('all');
   const { openpeepsApi } = useOpenpeeps();
   const { t } = useTranslation();
   const query = openpeepsApi.useCurrentProfileNotifications({
     limit: 15,
   });
 
-  const markAllNotificationsAsSeen = openpeepsApi.markAllNotificationsAsSeenAction();
+  const markAllNotificationsAsSeen =
+    openpeepsApi.markAllNotificationsAsSeenAction();
 
   markAllNotificationsAsSeen()().then(() => {
     setAppBadgeCount(0);
@@ -25,31 +24,16 @@ export const Notifications = () => {
 
   return (
     <ThemedView style={{ flexGrow: 1 }}>
-      <Tabs
-        value={tabValue}
-        onValueChange={setTabValue}
-        className="w-full mx-auto flex-col gap-1.5">
-        <TabScreensHeader
-          children={
-            <View>
-              <ThemedText className="text-xl font-bold">
-                {t('navigation.notifications')}
-              </ThemedText>
-              <TabsList className="flex-row w-full bg-transparent border-muted rounded-none border-b p-0 px-3">
-                <TabsTrigger
-                  value="all"
-                  className={`${tabValue === 'all' ? 'border-b-2 border-foreground' : ''
-                    }`}>
-                  <ThemedText>All</ThemedText>
-                </TabsTrigger>
-              </TabsList>
-            </View>
-          }
-        />
-        <TabsContent value="all" className="w-full p-0">
-          <NotificationFeed query={query} />
-        </TabsContent>
-      </Tabs>
+      <TabScreensHeader
+        children={
+          <View>
+            <ThemedText className="text-xl font-bold">
+              {t('navigation.notifications')}
+            </ThemedText>
+          </View>
+        }
+      />
+      <NotificationFeed query={query} />
     </ThemedView>
   );
 };
