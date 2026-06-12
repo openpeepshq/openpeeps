7f95b7e24fcdaa88b1f5ac72c92c9062895fdfd7
Jeremiah-Quaynor <hommeyj7@gmail.com>
2026-06-12 11:11:14 +0000

    fix(react): added Inter font and update styles

--- platform/web/src/main.tsx
+++ platform/web/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
 
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 import './styles.css';
 
 const root = document.getElementById('root');
